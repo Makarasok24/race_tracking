7622e706d1e0853ccece9808267131affb4ce167
Makarasok24 <makarasok.it@gmail.com>
2025-05-10 13:24:06 +0700

    race card

--- lib/core/constants/firebase_config.dart
+++ lib/core/constants/firebase_config.dart
@@ -1,3 +1,6 @@
 class FirebaseConfig {
-  static const String baseUrl = 'https://week8-firebase-d580e-default-rtdb.asia-southeast1.firebasedatabase.app/';
+  static const String baseUrl =
+      'https://week8-firebase-d580e-default-rtdb.asia-southeast1.firebasedatabase.app/';
+  // static const String baseUrl =
+  //     'https://week-8-practice-3484f-default-rtdb.asia-southeast1.firebasedatabase.app/';
 }
--- lib/presentation/widgets/segment_card.dart
+++ lib/presentation/widgets/segment_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:race_tracking/presentation/theme/theme.dart';
+import 'package:race_tracking/presentation/widgets/status_badge.dart';
 
 class SegmentCard extends StatelessWidget {
   final String title;
@@ -33,55 +34,49 @@ class SegmentCard extends StatelessWidget {
       elevation: 4,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: Row(
           children: [
-            Image.asset(imagePath, width: 80, height: 80, fit: BoxFit.contain),
+            Image.asset(imagePath, width: 80, height: 80),
             const SizedBox(width: 16),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                          color: statusColor,
-                          shape: BoxShape.circle,
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(width: 8),
+                      StatusBadge(status: status),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
                       Text(
-                        status,
-                        style: TextStyle(
-                          color: statusColor,
+                        "$participantCount participants",
+                        style: const TextStyle(
+                          color: Colors.grey,
                           fontWeight: FontWeight.w500,
                         ),
                       ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: RTAColors.primary,
+                        size: 20,
+                      ),
                     ],
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "$participantCount participants",
-                    style: const TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w500,
-                    ),
                   ),
                 ],
               ),
             ),
-            Icon(Icons.arrow_forward_ios, color: RTAColors.primary, size: 20),
           ],
         ),
       ),
